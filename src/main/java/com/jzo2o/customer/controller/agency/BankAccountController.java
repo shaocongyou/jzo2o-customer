3297595aa76c9bad0139703f44c4a6733a672b99
Shaocong You <shaocongyou464@gmail.com>
2025-07-03 14:46:09 +0800

    服务端 + 机构端   服务者 + 机构者   获取银行账户

--- src/main/java/com/jzo2o/customer/controller/agency/BankAccountController.java
+++ src/main/java/com/jzo2o/customer/controller/agency/BankAccountController.java
@@ -1,14 +1,14 @@
 package com.jzo2o.customer.controller.agency;
 
+import com.jzo2o.common.utils.BeanUtils;
+import com.jzo2o.customer.model.domain.BankAccount;
 import com.jzo2o.customer.model.dto.request.BankAccountUpsertReqDTO;
+import com.jzo2o.customer.model.dto.response.BankAccountResDTO;
 import com.jzo2o.customer.service.IBankAccountService;
 import com.jzo2o.mvc.utils.UserContext;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -25,5 +25,13 @@ public class BankAccountController {
     public void addBankAccount(@RequestBody BankAccountUpsertReqDTO bankAccountUpsertReqDTO) {
         bankAccountUpsertReqDTO.setId(UserContext.currentUserId());
         bankAccountService.saveOrUpdate(bankAccountUpsertReqDTO);
+    }
+
+    @GetMapping("/currentUserBankAccount")
+    @ApiOperation("获取银行账户")
+    public BankAccountResDTO getBankAccount() {
+        BankAccount bankAccount = bankAccountService.getById(UserContext.currentUserId());
+        BankAccountResDTO bankAccountResDTO = BeanUtils.toBean(bankAccount, BankAccountResDTO.class);
+        return bankAccountResDTO;
     }
 }
